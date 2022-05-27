baad84cf7ea86c2aac4e6645abc2ee50479866c8
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-05-27 15:16:02 -0500

    Add: task 1-hbnb.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,7 +1,17 @@
 window.onload = function () {
-  debugger
-  var textinputs = document.querySelectorAll(".popover li input[type=checkbox]")
-  console.log(textinputs)
-  var checkbox = (Array.from(textinputs)).filter((element)=> element.checked === true);
-  console.log(checkbox)
+  let amenities = {}
+//  debugger
+  $('input[type="checkbox"]').change(function() {
+    if (this.checked) {
+      amenities[$(this).data('id')] = ($(this).data('name'));
+    } else {
+      delete amenities[($(this).data('id'))];
+    }
+    //$('#h4_amenities').text(Object.values(amenities))
+    $('#h4_amenities').text(Object.keys(amenities).map(function(k){return amenities[k]}).join(", "));
+  });
 }
+//  var textinputs = document.querySelectorAll(".popover li input[type=checkbox]")
+//  console.log(textinputs)
+//  var checkbox = (Array.from(textinputs)).filter((element)=> element.checked === true);
+//  console.log(checkbox)
