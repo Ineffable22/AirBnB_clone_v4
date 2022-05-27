4335e20c09a69f40c598f659cf9ffc115e31eb1e
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-05-26 20:01:45 -0500

    Feat(3-python_route.py): code was updated

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
-from flask import Flask
+"""This script starts a Flask web application"""
+from flask import Flask, escape
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+def route():
+    """Return two words"""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """returns HBNB"""
-    return 'HBNB'
+def route_hbnb():
+    """Return a word"""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+@app.route('/c/<path:subpath>', strict_slashes=False)
+def route_c(subpath):
+    """Return subpath"""
+    return "C {}".format(escape(subpath).replace('_', ' '))
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
-    return 'Python ' + text.replace('_', ' ')
+@app.route('/python', defaults={'subpath': 'is cool'}, strict_slashes=False)
+@app.route('/python/<path:subpath>', strict_slashes=False)
+def route_python(subpath):
+    """Return subpath"""
+    return "Python {}".format(escape(subpath).replace('_', ' '))
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host="0.0.0.0", port=5000)
