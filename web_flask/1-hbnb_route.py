1d4b741f1b0427f023082ed2fa2fcca2ab29cd44
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-05-26 20:01:38 -0500

    Feat(1-hbnb_route.py): code was updated

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
+""" This script starts a Flask web application """
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+def hello():
+    """Return two words"""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """returns HBNB"""
-    return 'HBNB'
+def hello2():
+    """Return a word"""
+    return "HBNB"
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host="0.0.0.0", port=5000)
