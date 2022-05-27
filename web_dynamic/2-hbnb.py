4657cbae273e1dcaaad1c3cb760488939d126d13
cristhian1107 <cristhian.cjaa@gmail.com>
2022-05-27 16:43:46 -0500

    Feat (Add) Status Api

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -9,13 +9,13 @@ import uuid
 app = Flask(__name__)
 
 
-@app.route('/1-hbnb', strict_slashes=False)
+@app.route('/2-hbnb', strict_slashes=False)
 def hbnb():
     """/hbnb: display a HTML page with render_template"""
     states = storage.all('State').values()
     amenities = storage.all('Amenity').values()
     places = storage.all('Place').values()
-    return render_template('1-hbnb.html',
+    return render_template('2-hbnb.html',
                            states=states,
                            amenities=amenities,
                            places=places,
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   let amenities = {}
-//  debugger
+  // debugger
   $('input[type="checkbox"]').change(function() {
     if (this.checked) {
       amenities[$(this).data('id')] = ($(this).data('name'));
@@ -10,6 +10,17 @@ window.onload = function () {
     //$('#h4_amenities').text(Object.values(amenities))
     $('#h4_amenities').text(Object.keys(amenities).map(function(k){return amenities[k]}).join(", "));
   });
+
+  $.get('http://127.0.0.1:5001/api/v1/status/', function (data, status) {
+    // console.log(status)
+    // console.log(data.status)
+    if (data.status === 'OK'){
+      $('#api_status').addClass('available')
+    }
+    else {
+      $('#api_status').removeClass('available')
+    }
+});
 }
 //  var textinputs = document.querySelectorAll(".popover li input[type=checkbox]")
 //  console.log(textinputs)
